98bd5a7cefb1ff6e20df55d6c484bc231a8e8d02
Davidkity <541981334@qq.com>
2020-05-26 15:37:40 +0800

    断路器Hystrix 服务降级、服务熔断

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/lzm/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,46 @@
+package com.lzm.springcloud.controller;
+
+import com.lzm.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
+public class OrderHystrixController {
+
+    @Autowired
+    private PaymentHystrixService paymentHystrixService;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id){
+        return paymentHystrixService.paymentInfo_OK(id);
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")  //3秒钟以内就是正常的业务逻辑
+//    })
+    // 上方是定制专属的降级服务（可配置到外面接口的实现类中，避免业务混乱）
+    @HystrixCommand
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        int i = 10/0;
+        return paymentHystrixService.paymentInfo_TimeOut(id);
+    }
+
+    //兜底方法
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
+        return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
+    }
+
+    //全局fallback
+    public String payment_Global_FallbackMethod(){
+        return "Global异常处理信息，请稍后再试，o(╥﹏╥)o~~";
+    }
+}
