ee466684a8f0798b2d243e837f4fcc8ca06efcbf
Davidkity <541981334@qq.com>
2020-05-26 18:15:57 +0800

    GateWay 网关（Route路由、Predicate断言、Filter过滤）

---
+++ cloud-gateway-gateway9527/src/test/java/getTime.java
@@ -0,0 +1,9 @@
+import java.time.ZonedDateTime;
+
+public class getTime {
+    public static void main(String[] args) {
+        ZonedDateTime time = ZonedDateTime.now();
+        System.out.println(time);
+    //  2020-05-26T17:39:25.471+08:00[GMT+08:00]
+    }
+}
